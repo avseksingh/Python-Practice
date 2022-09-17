0150ea4de054da12ae83cb39354d6410a0ab1056
Abhishek Singh <avsekjob@gmail.com>
2022-09-17 14:59:07 +0530

    .

--- anagramstring.py
+++ anagramstring.py
@@ -1,10 +1,27 @@
-a = "geeksforgeeks"
-b = "forgeeksgeeks"
+a1 = "geeksforgeeks"
+b1 = "forgeeksgeeks"
+a = ''.join(sorted(a1))
+b = ''.join(sorted(b1))
+flag = False
+if len(a1) == len(b1):
+    for i in range(len(a)):
+        if a[i] == b[i]:
+            flag = True
+        else:
+            flag = False
+            break
+if flag:
+    print("String is Anagram")
+else:
+    print("string is not an anagram")
 
-if len(a) == len(b):
-    for i in range(len(a) - 1):
-        for e in b:
-            if a[i] == b:
-                a.pop(i)
 
-print(a)
+
+
+# if len(a) == len(b):
+#     for i in range(len(a) - 1):
+#         for e in b:
+#             if a[i] == b:
+#                 a.pop()
+#
+# print(a)
